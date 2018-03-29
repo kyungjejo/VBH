fd5c8124431aabdfa7d2871e3b0df7e47dc84cfd
kyungjejo <kyungjejo@gmail.com>
2018-03-29 22:48:39 +0900

    first commit

--- frontend/app/src/static/무제 폴더/extract.sh
+++
@@ -1,4 +0,0 @@
-for file in ../video/*.mp4; do 
-	mkdir "${file%.*}"
-	ffmpeg -i "$file" -f image2 -vsync vfr -vf "[in]select='eq(pict_type,PICT_TYPE_I)',showinfo[out]" "${file%.*}"/thumb%04d.png 2> "$file"_positions.txt
-done
